7395075f9000b1d44dbf204eedef33e57c787a4d
MR DULEN <146187447+MR-DULEN@users.noreply.github.com>
2025-02-02 23:10:55 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,24 +4,24 @@ const fs = require('fs');
 const chalk = require('chalk');
 
 //owmner v card
-global.ytname = "YT: GlobalTechInfo" //ur yt chanel name
-global.socialm = "GitHub: GlobalTechInfo" //ur github or insta name
-global.location = "Pakistan, Karachi, Karachi" //ur location
+global.ytname = "YT: MR DULEN" //ur yt chanel name
+global.socialm = "GitHub: MR DULEN" //ur github or insta name
+global.location = "Sri Lanka, Southern, Hambanthota" //ur location
 
 //new
 global.botname = '© 𝐆𝐋𝐎𝐁𝐀𝐋-𝐌𝐃' //ur bot name
-global.ownernumber = ['923444844060'] //ur owner number, dont add more than one
-global.ownername = '© GlobalTechInfo' //ur owner name
-global.websitex = "https://www.youtube.com/@GlobalTechInfo"
+global.ownernumber = ['94778023384'] //ur owner number, dont add more than one
+global.ownername = '© Mr xDulen' //ur owner name
+global.websitex = "https://mr-dulen.github.io"
 global.wagc = "https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07"
 global.themeemoji = '🪀'
 global.wm = "Global Bot Inc."
 global.botscript = 'https://github.com/GlobalTechInfo/GLOBAL-MD' //script link
 global.packname = "Sticker By"
-global.author = "GlobalTechInfo\n\n+923444844060"
-global.creator = "923444844060@s.whatsapp.net"
+global.author = "MR xDulen\n\n+94778023384"
+global.creator = "94778023384@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["923444844060"] // Premium User
+global.premium = ["94778023384"] // Premium User
 
 //channel id
 global.xchannel = {
@@ -31,15 +31,15 @@ global.xchannel = {
 //bot sett
 global.typemenu = 'v12' // menu type 'v1' => 'v12'
 global.typereply = 'v4' // reply type 'v1' => 'v4'
-global.autoblocknumber = '91' //set autoblock country code
-global.antiforeignnumber = '91' //set anti foreign number country code
+global.autoblocknumber = '' //set autoblock country code
+global.antiforeignnumber = '' //set anti foreign number country code
 
 global.listv = ['➜','☞','㉨','✪','✯','益','⊡','☆','ෆ','✎','⎆','⫹⫺','■','✿','▲','➩','➢','➣','➤','✦','✧','△','❀','○','□','♤','♡','◇','♧','々','〆','֎','ᚏ','⸙','⧉']
 global.tempatDB = 'database.json'
 
 global.limit = {
-	free: 100,
-	premium: 999,
+	free: 10000,
+	premium: 99999,
 	vip: 'VIP'
 }
 
